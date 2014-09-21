a4644c9c885d8e742104680a5a25fee73ef91da7
Garth Cumming <garthman@gmail.com>
2014-09-20 19:23:01 -0700

    Removed old script

--- new-instance.sh
+++
@@ -1,5 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PASS=$1
-DOMAIN=${2:-talk.hackspace.ca}
-SELECTOR=${3:-default}
-docker run -e "domain=$DOMAIN" -e "selector=$SELECTOR" -e "passwd=$PASS" -d -p 110:110 -p 25:25 -v $DIR/log:/var/log/supervisor -v $DIR/mail:/var/mail -v $DIR/home:/home --name discourse_mail hackspace/discourse_mail
